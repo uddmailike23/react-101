e7da2573b4e22652e13469f7da9ec43000262352
DeCrow <nonchana.s23@gmail.com>
2019-06-02 00:05:35 +0700

    setState

--- src/components/show-name.js
+++ src/components/show-name.js
@@ -1,15 +1,33 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react'
 
 class ShowName extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      name: 'Lilly'
+      name: 'Lilly',
+      displayDate: new Date().toString()
     }
   }
 
+  componentDidMount() {
+    this.interval = setInterval(this.updateDisplayDate, 1000)
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interval)
+  }
+
+  updateDisplayDate = () => {
+    this.setState({ displayDate: new Date().toString() })
+  }
+
   render() {
-    return <p>My sname is {this.state.name}</p>
+    return (
+      <Fragment>
+        <p>My sname is {this.state.name}</p>
+        <p>{this.state.displayDate}</p>
+      </Fragment>
+    )
   }
 }
 
